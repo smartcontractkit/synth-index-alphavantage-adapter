c573ddcc6605c7291b5d719a057ba3607d2d3cd1
Thomas Hodges <me@thodges.com>
2020-03-17 09:23:15 -0500

    Handle errors from API and in index calculation

--- index.js
+++ index.js
@@ -1,83 +1,72 @@
-const rp = require('request-promise');
-const snx = require('synthetix');
-const Decimal = require('decimal.js');
-
-function sleep(ms) {
-  return new Promise(resolve => {
-    setTimeout(resolve, ms)
-  })
-}
+const rp = require('request-promise')
+const snx = require('synthetix')
+const Decimal = require('decimal.js')
 
 const getPriceData = async (synth) => {
-  await sleep(10000);
-
-  return rp({
-    url: `https://www.alphavantage.co/query`,
-    qs: {
-      function: "CURRENCY_EXCHANGE_RATE",
-      from_currency: synth,
-      to_currency: "USD",
-      apikey: process.env.API_KEY
-    },
-    json: true
-  })
-};
+  try {
+    return await rp({
+      url: `https://www.alphavantage.co/query`,
+      qs: {
+        function: 'CURRENCY_EXCHANGE_RATE',
+        from_currency: synth,
+        to_currency: 'USD',
+        apikey: process.env.API_KEY
+      },
+      json: true
+    })
+  } catch (error) {
+    throw error.message
+  }
+}
 
 const calculateIndex = (indexes) => {
-  let value = new Decimal(0);
-  indexes.forEach(i => {
-    const price = i.priceData["Realtime Currency Exchange Rate"]["5. Exchange Rate"];
-    if (price <= 0)
-      throw "invalid price";
-    value = value.plus(new Decimal(i.units).times(new Decimal(price)));
-  });
+  let value = new Decimal(0)
+  try {
+    indexes.forEach(i => {
+      value = value.plus(new Decimal(i.units).times(new Decimal(i.priceData['Realtime Currency Exchange Rate']['5. Exchange Rate'])))
+    })
+  } catch (error) {
+    throw error.message
+  }
   return value.toNumber()
-};
+}
 
 const createRequest = async (input, callback) => {
-  const asset = input.data.asset || 'sCEX';
-  const datas = snx.getSynths({network: 'mainnet'}).filter(({index, inverted}) => index && !inverted);
-  const data = datas.find(d => d.name.toLowerCase() === asset.toLowerCase());
-  await Promise.all(data.index.map(async (synth) => {
-    synth.priceData = await getPriceData(synth.symbol)
-  })).catch(err => {
+  const asset = input.data.asset || 'sCEX'
+  const datas = snx.getSynths({network: 'mainnet'}).filter(({index, inverted}) => index && !inverted)
+  const data = datas.find(d => d.name.toLowerCase() === asset.toLowerCase())
+  try {
+    await Promise.all(data.index.map(async (synth) => {
+      synth.priceData = await getPriceData(synth.symbol)
+    }))
+    data.result = calculateIndex(data.index)
+    callback(200, {
+      jobRunID: input.id,
+      data: data,
+      result: data.result,
+      statusCode: 200
+    })
+  } catch (error) {
     callback(500, {
       jobRunID: input.id,
-      error: err,
+      status: 'errored',
+      error: error,
       statusCode: 500
     })
-  });
-
-  try {
-    data.result = calculateIndex(data.index);
-  } catch (e) {
-    callback(500, {
-      jobRunID: input.id,
-      error: "failed getting price",
-      statusCode: 500
-    });
-    return
   }
-
-  callback(200, {
-    jobRunID: input.id,
-    data: data,
-    result: data.result,
-    statusCode: 200
-  })
-};
+}
 
 exports.gcpservice = (req, res) => {
   createRequest(req.body, (statusCode, data) => {
     res.status(statusCode).send(data)
   })
-};
+}
 
 exports.handler = (event, context, callback) => {
   createRequest(event, (statusCode, data) => {
     callback(null, data)
   })
-};
+}
 
 exports.handlerv2 = (event, context, callback) => {
   createRequest(JSON.parse(event.body), (statusCode, data) => {
@@ -87,6 +76,6 @@ exports.handlerv2 = (event, context, callback) => {
       isBase64Encoded: false
     })
   })
-};
+}
 
-module.exports.createRequest = createRequest;
+module.exports.createRequest = createRequest
